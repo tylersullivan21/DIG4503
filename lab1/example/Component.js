46e4ec8d3b00bae9bf8e118c19636b18e80571a8
Tyler Sullivan <tyler.sullivan2198@knights.ucf.edu>
2020-01-13 19:09:55 -0500

    working on component examples

---
+++ lab1/example/Component.js
@@ -0,0 +1,12 @@
+class Component{
+
+    constructor(){
+        this.text
+        this.links
+        this.images
+    }
+
+   
+}
+
+module.exports = Componenet;
---
+++ lab1/example/Header.js
@@ -0,0 +1,12 @@
+const Component = require("./Component.js");
+
+class Header extends Component {
+constructor(){
+    super();
+    this.text = "Oh, hi";
+}
+
+
+}
+
+module.exports = Header;
