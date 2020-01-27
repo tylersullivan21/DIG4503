5968678cc12ac8b392adcaeab2f936bdd0e7e9f5
Tyler Sullivan <tyler.sullivan2198@knights.ucf.edu>
2020-01-27 18:56:32 -0500

    getting fancy

---
+++ express-example/Person.js
@@ -0,0 +1,16 @@
+class Person{
+
+    constructor(name, color){
+        this.name = name;
+        this.color = color;
+    }
+
+    getName(){
+        return this.name;
+    }
+    getColor(){
+        return this.color;
+    }
+}
+
+module.exports = Person
--- express-example/index.js
+++ express-example/index.js
@@ -2,10 +2,16 @@ const Express = require("express");
 const App = Express();
 const port = 8080;
 
+const Person = require("./Person");
+
+let p = new Person("Tyler", "Magenta");
+
+let db = [5, 24, 25, 16];
 
 App.get("/", function(Request, Response){
 
-Response.send("<p>Hi</p>");
+/*Response.json({"db": db[1]});*/
+Response.send(p);
 
 })
 
