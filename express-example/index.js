d0cea6dffff0225372afce866f4c8e89654b5d5f
Tyler Sullivan <tyler.sullivan2198@knights.ucf.edu>
2020-01-27 18:28:18 -0500

    working on server ports

--- express-example/index.js
+++ express-example/index.js
@@ -1,13 +1,20 @@
 const Express = require("express");
 const App = Express();
+const port = 8080;
 
 
-App.get("/", function(){
+App.get("/", function(Request, Response){
 
-console.log("Request!");
+Response.send("<p>Hi</p>");
 
 })
-App.listen(80, function(){
+
+App.get("/book", function(Request, Response){
+    Response.send("Book");
+})
+
+
+App.listen(port, function(){
 
 
 console.log("Server Running");
