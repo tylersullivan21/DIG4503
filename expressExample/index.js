0f9ccb2c2b3e4785e63300aea75f1f5e9d4a2ef6
Tyler Sullivan <tyler.sullivan2198@knights.ucf.edu>
2020-01-29 19:04:43 -0500

    played with react in express

--- expressExample/index.js
+++ expressExample/index.js
@@ -2,13 +2,15 @@ const Express = require("express");
 const App = Express();
 const port = 80;
 
-
+/*
 App.use(Express.static("static"));
+*/
+App.use(Express.static("client/build"));
 
 App.use("/anotherFolder", Express.static("anotherFolder"));
 
 App.get("/api", function(req,res){
-    res.send("Hi");
+    res.send("Hi vivien");
 });
 
 
