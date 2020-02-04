8d05e48cad51c014e2794e575591de0f02e41b59
Tyler Sullivan <tyler.sullivan2198@knights.ucf.edu>
2020-02-03 19:05:01 -0500

    in class example

---
+++ dynamicexpress/Person.js
@@ -0,0 +1,14 @@
+class Person{
+
+constructor(name, color){
+
+    this.name = name;
+    this.color = color;
+
+
+}
+
+}
+
+
+module.exports = Person;
--- dynamicexpress/index.js
+++ dynamicexpress/index.js
@@ -2,11 +2,43 @@ const Express = require("express");
 const App = Express();
 const port = 4000;
 
-App.get("/:book:/id", (req,res) => {
-res.send(req.params);
+
+
+const Person = require("./Person.js")
+let p = new Person("Dan", "green");
+let people[];
+
+const faker = require("faker");
+let name = faker.name.findName();
+let color = faker.color.findName();
+
+for(let i = 0; i< 99; i++)
+{
+
+   
+
+    people.push(new Person());
+}
+
+
+App.get("/person/name/:name", (req,res) => {
+    people.forEach((value) =>{
+            if(value.name == req.params.name){
+                    res.send(value)
+            }
+    });
+
+    res.send("Hey ont you)
+
+res.send(req.params.name);
 
 });
 
+App.get("/person/color/:color", (req,res) => {
+    res.send(req.params.color);
+    
+    });
+
 App.listen(port, () => {
     console.log("Server Running");
-})
+});
