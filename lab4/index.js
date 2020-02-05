ee9ee54cbe6203d6872f0f85ed01d738215ee5f0
Tyler Sullivan <tyler.sullivan2198@knights.ucf.edu>
2020-02-05 15:10:43 -0500

    added code for id and name

--- lab4/index.js
+++ lab4/index.js
@@ -1,12 +1,32 @@
 const Express = require("express");
 const App = Express();
 const port = 8080;
-const Person = require("./Pokemon");
+const Pokemon = require("./Pokemon");
 const faker = require("faker");
 
-App.get('/', function(request,response){
-    response.send("Hello World!");
-})
+App.get("/pokemon/id/:id", (req, res) => {
+    people.forEach((value)=> {
+        if(value.name == req.params.id) {
+            res.send(value);
+        }
+    });
+
+    res.send("Invalid!");
+
+});
+
+
+
+App.get("/pokemon/name/:name", (req, res) => {
+    people.forEach((value)=> {
+        if(value.color == req.params.name) {
+            res.send(value);
+        }
+    });
+
+    res.send("Invalid!");
+
+});
 
 App.listen(port,() => {
     console.log("Server is running");
