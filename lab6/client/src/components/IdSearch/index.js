7c2e11c53f8cba6374e050642529b64ec08d1baa
tylersullivan21 <tyler.sullivan2198@knights.ucf.edu>
2020-02-20 13:48:44 -0500

    added id search

--- lab6/client/src/components/IdSearch/index.js
+++ lab6/client/src/components/IdSearch/index.js
@@ -2,8 +2,48 @@ import React from 'react';
 
 class IdSearch extends React.Component{
 
+ readId(){
 
     
+        let pokemonId = document.querySelector("#id");
+
+//need to figure out how to patch api
+        fetch("/id/" + pokemonId.value)
+        .then((res) =>{
+
+        return res.json();
+        })
+    .then((processed) =>{
+
+         let outputArea= document.querySelector("#reportingArea");
+
+
+        if(processed.error){
+            outputArea.innerHTML = processed.error;
+        }
+        else{
+            outputArea.innerHTML = processed.name;
+            /*
+                        outputArea.innerHTML = processed.id+ ": "+ processed.name + " : "+ processed.typeList;
+                        */
+        }
+ });
+
+}
+
+render() {
+    return(
+        <div>
+             <h2>Name</h2>
+             <form onSubmit={this.readId}>
+                <input id="id" type="text" />
+                <button>Submit</button>
+            </form>
+        </div>
+    );
+}
+
+
 }
 
 
