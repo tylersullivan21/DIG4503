73cc24fc18215052846c5dddba083270f659f353
Tyler Sullivan <tyler.sullivan2198@knights.ucf.edu>
2020-02-24 18:46:31 -0500

    creating database in construction

---
+++ mongoexample/database.js
@@ -0,0 +1,38 @@
+
+class Database {
+    constructor(){
+ 
+
+    const MongoClient = require("mongodb").MongoClient;
+
+const URl = "mongodb+srv://<jamessullivan>:<NXa6AwWqCvtcsxsq>@cluster0-tc9sb.mongodb.net/";
+
+MongoClient.connect(URL, function(error, connection) {
+
+if (error){
+    throw error;
+}
+
+let database = connection.db("DIG4503-78");
+let collection = database.collection("Movies");
+
+console.log(collection);
+
+collection.findOne({
+    "score": 79
+}, {}, (error, result) => {
+console.log(result)
+});
+
+console.log(result);
+
+connection.close();
+
+
+} );
+    }
+}
+
+
+
+
