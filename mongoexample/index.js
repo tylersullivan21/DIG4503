c3cad9671ad671325311e8fa2488f2d7f9331f1a
Tyler Sullivan <tyler.sullivan2198@knights.ucf.edu>
2020-02-24 18:33:36 -0500

    added callback function

--- mongoexample/index.js
+++ mongoexample/index.js
@@ -1,4 +1,4 @@
-const MongoDBClient = require("mongodb").MongoClient;
+const MongoClient = require("mongodb").MongoClient;
 
 const URl = "mongodb+srv://<jamessullivan>:<NXa6AwWqCvtcsxsq>@cluster0-tc9sb.mongodb.net/";
 
@@ -15,6 +15,14 @@ let database = connection.db("DIG4503-78");
 let collection = database.collection("Movies");
 
 console.log(collection);
+
+collection.findOne({
+    "score": 79
+}, {}, (error, result) => {
+console.log(result)
+});
+
+console.log(result);
 
 
 connection.close();
