f75e8dcbdf4870137e9e81f161964b2d96bb7a04
Tyler Sullivan <tyler.sullivan2198@knights.ucf.edu>
2020-03-02 18:28:46 -0500

    added collection

--- compassDB/index.js
+++ compassDB/index.js
@@ -2,6 +2,24 @@ const Express = require('Express');
 const App = Express();
 const port = 80;
 
+
+const MonogDB = require('');
+const mongoDBClient = MongoDB.MongoClient;
+const URL = "";
+
+
+let collection = null;
+
+MongoClient.connect(URL, (error, connection) => {
+if(error){
+    throw error;
+}
+
+connection.db("DIG4503-78");
+collection = database.collection("Movies");
+
+});
+
 const CORS = require('cors');
 App.use(CORS());
 
@@ -10,8 +28,13 @@ App.listen(port, () => {
 
 App.get("movies/title/:title", (req,res) => {
 
+    let result = {"error": "Could not find movies!"};
 
+if(collection != null){
+    collection.find();
+}
 
+    res.json(result);
 
 })
 
