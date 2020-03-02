32427468a1e1173f06bd3776b24d1877685d4bf6
Tyler Sullivan <tyler.sullivan2198@knights.ucf.edu>
2020-03-02 18:15:58 -0500

    added cors and use and listen

---
+++ compassDB/index.js
@@ -0,0 +1,19 @@
+const Express = require('Express');
+const App = Express();
+const port = 80;
+
+const CORS = require('cors');
+App.use(CORS());
+
+
+App.listen(port, () => {
+
+App.get("movies/title/:title", (req,res) => {
+
+
+
+
+})
+
+
+});
