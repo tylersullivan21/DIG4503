e3b93954a52c11b096830f1c80794dcdd26f2862
tylersullivan21 <tyler.sullivan2198@knights.ucf.edu>
2020-03-06 09:15:47 -0500

    added YearSearch component

--- lab7/client/src/App.js
+++ lab7/client/src/App.js
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import YearSearch from './components/YearSearch';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      "table": []
+    };
+
+  }
+
+  // Update the internal state.table
+  // (This is called from YearSearch.)
+  setResultsTable = (processed) => {
+    this.setState({"table": processed});
+  }
+
+  // Render the results table
+  createResultsTable() {
+
+    return (
+      <table>
+        <tbody>
+        {
+          this.state.table.map((entry, index) => {
+             return( 
+              <tr key={index}>
+                <td>{entry.title}</td>
+                <td>{entry.year}</td>
+              </tr>
+             );
+          })
+        }
+        </tbody>
+      </table>
+    );
+
+  }
+
+  render() {
+    return (
+      <div>
+        <YearSearch callback={this.setResultsTable} />
+        {this.createResultsTable()}
+      </div>
+    );
+  }
+  
 }
 
 export default App;
