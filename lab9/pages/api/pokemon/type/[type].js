895904d3635a2a163c216f3a868c0cd70dbf854a
tylersullivan21 <tyler.sullivan2198@knights.ucf.edu>
2020-03-27 12:11:45 -0400

    working on lab 9

--- lab9/pages/api/pokemon/type/[type].js
+++ lab9/pages/api/pokemon/type/[type].js
@@ -1,30 +1,18 @@
 import pokemon from 'json-pokemon/pokemon';
 
-function response(req,res){
+export default  (req,res) => {
+
+    let result = {"error": "Could not find pokemon type."};
+
+ 
 for(let i=0; i< pokemon.length; i++) {
 
     for(let k=0; k < pokemon[i].typeList.length; k++){
         
+
+
     }
 
 }
-
-
-}
-
-
-
-/*
-export default (req, res) => {
-
-    let result = {"error": "Could not find name."};
-
-    let pokemon = getPokemon.getPokemonByType(req.query.type);
-
-    if(pokemon !== null) {
-        result = pokemon;
-    }
-
-    res.json(result);
 
 }
