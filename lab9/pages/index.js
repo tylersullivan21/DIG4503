254cb174b5527e45c530dfa20860cb0fea56711f
Tyler Sullivan <tyler.sullivan2198@knights.ucf.edu>
2020-03-28 21:34:50 -0400

    added styles to home page

--- lab9/pages/index.js
+++ lab9/pages/index.js
@@ -1,6 +1,5 @@
-import NameSearch from "../components/NameSearch/index";
-import IdSearch from "../components/IdSearch/index";
 
+import styles from"./nav.module.css";
 
 import Head from 'next/head';
 import Link from 'next/link';
@@ -14,21 +13,22 @@ return(
   <Head>
 <title>This is a test title</title>
     </Head>
-          <p>Test</p>
+          <p className={styles.para}>Welcome Poke Trainer! Check out some of your fave pokemon by name, id and type!</p>
 
+        <div className={styles.navcontainer}>
           <Link href="/namesearch">
-          <a>name search</a>
+          <button className={styles.button}><a className={styles.left} >name search</a></button>
           </Link>
-<br>
-</br>
+
           <Link href="/idsearch">
-          <a>id search</a>
+          <button className={styles.button}><a className={styles.center} >id search</a></button>
           </Link>
 
           <Link href="/typesearch">
-          <a>type search</a>
+          <button className={styles.button}><a className={styles.right} >typesearch</a></button>
           </Link>
 
+          </div>
           </div>
   </div>
 
